5d675b9cbf4def9224f88cace4dc7934c3c281ed
“MasterGodzilla” <ryansunhanchi@gmail.com>
2024-07-29 14:47:05 -0400

    33b analysis

--- EAGLE/analyze.sh
+++ EAGLE/analyze.sh
@@ -1,7 +1,7 @@
 # for every file in mt_bench folder, we run the eagle.evaluation.accept_length python script
 # then, we save the output to log.txt file along with the file name
 
-for file in mt_bench/*; do
-    echo $file >> log.txt
-    python eagle/evaluation/accept_length.py --jsonl_file $file >> log.txt
+for file in mt_bench/33b/*; do
+    echo $file >> log33b.txt
+    python eagle/evaluation/accept_length.py --jsonl_file $file >> log33b.txt
 done
