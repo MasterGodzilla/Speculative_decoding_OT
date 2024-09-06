7d592b70e954bb1fb48516a5628f9c3439645464
MasterGodzilla <79106280+MasterGodzilla@users.noreply.github.com>
2024-09-06 11:20:10 -0400

    Delete gpt-fast directory

--- gpt-fast/convert.sh
+++
@@ -1,6 +0,0 @@
-# karpathy/tinyllamas
-
-# export MODEL_REPO=meta-llama/Llama-2-7b-chat-hf
-source activate /home/hanchi/miniconda3/envs/gpt-fast
-export MODEL_REPO=TinyLlama/TinyLlama-1.1B-Chat-v1.0
-python scripts/download.py --repo_id $MODEL_REPO &&  python scripts/convert_tinyllama.py --checkpoint_dir checkpoints/$MODEL_REPO && python quantize.py --checkpoint_path checkpoints/$MODEL_REPO/model.pth --mode int8
